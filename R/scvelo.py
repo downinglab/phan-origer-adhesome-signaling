575a9959e7fba2c0889cf011ce31883a49389cd5
Nolan Origer <nolanoriger@protonmail.com>
2026-07-16 13:29:33 -0700

    Update scvelo.py

--- R/scvelo.py
+++ R/scvelo.py
@@ -1,3 +1,6 @@
+# Figure 5 scVelo RNA velocity analysis
+# By Andrew Phan
+
 import scvelo as scv
 import scanpy as sc
 import cellrank as cr
@@ -9,17 +12,18 @@ scv.settings.verbosity = 3
 scv.settings.set_figure_params('scvelo', facecolor='white', dpi=100, frameon=False)
 cr.settings.verbosity = 2
 
-adata = sc.read_h5ad('/home/data/aqphan/RStudio/DowningLab_Git/AQPhan/scRNA-seq/iPSC_Reprog/RNAvelocity/my_data.h5ad')
+# Need to convert to h5ad
+adata = sc.read_h5ad('~/converted-combined.h5ad')
 
 # load loom files for spliced/unspliced matrices for each sample:
-ldata1 = scv.read('/home/data/Shared/AQPhan/hiF-T+A549_scRNA-seq_11-16-20/Velocyto_Out/D6LacZ/possorted_genome_bam_7MVG3_D6LacZ.loom', cache=True)
-ldata2 = scv.read('/home/data/Shared/AQPhan/hiF-T+A549_scRNA-seq_11-16-20/Velocyto_Out/D6S3/possorted_genome_bam_GHOLO_D6S3.loom', cache=True)
-ldata3 = scv.read('/home/data/Shared/AQPhan/hiF-T+A549_scRNA-seq_11-16-20/Velocyto_Out/D9LacZ/possorted_genome_bam_EZ311_D9LacZ.loom', cache=True)
-ldata4 = scv.read('/home/data/Shared/AQPhan/hiF-T+A549_scRNA-seq_11-16-20/Velocyto_Out/D9S3/possorted_genome_bam_JLBHK_D9S3.loom', cache=True)
-ldata5 = scv.read('/home/data/Shared/AQPhan/hiF-T+A549_scRNA-seq_11-16-20/Velocyto_Out/D12LacZ/possorted_genome_bam_9IE2M_D12LacZ.loom', cache=True)
-ldata6 = scv.read('/home/data/Shared/AQPhan/hiF-T+A549_scRNA-seq_11-16-20/Velocyto_Out/D12S3/possorted_genome_bam_3WJ73_D12S3.loom', cache=True)
-ldata7 = scv.read('/home/data/Shared/AQPhan/hiF-T+A549_scRNA-seq_11-16-20/Velocyto_Out/D15LacZ/possorted_genome_bam_1QLD5_D15LacZ.loom', cache=True)
-ldata8 = scv.read('/home/data/Shared/AQPhan/hiF-T+A549_scRNA-seq_11-16-20/Velocyto_Out/D15S3/possorted_genome_bam_8JWL0_D15S3.loom', cache=True)
+ldata1 = scv.read('~/D6LacZ/possorted_genome_bam_7MVG3_D6LacZ.loom', cache=True)
+ldata2 = scv.read('~/D6S3/possorted_genome_bam_GHOLO_D6S3.loom', cache=True)
+ldata3 = scv.read('~/D9LacZ/possorted_genome_bam_EZ311_D9LacZ.loom', cache=True)
+ldata4 = scv.read('~/D9S3/possorted_genome_bam_JLBHK_D9S3.loom', cache=True)
+ldata5 = scv.read('~/D12LacZ/possorted_genome_bam_9IE2M_D12LacZ.loom', cache=True)
+ldata6 = scv.read('~/D12S3/possorted_genome_bam_3WJ73_D12S3.loom', cache=True)
+ldata7 = scv.read('~/D15LacZ/possorted_genome_bam_1QLD5_D15LacZ.loom', cache=True)
+ldata8 = scv.read('~/D15S3/possorted_genome_bam_8JWL0_D15S3.loom', cache=True)
 
 # rename barcodes in order to merge:
 barcodes = [bc.split(':')[1] for bc in ldata1.obs.index.tolist()]
